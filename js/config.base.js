d0c7b5ffb88055ff0f17e62463557ade3e76ac6e
New Author Name <email@address.example>
2024-12-09 00:00:26 +0000

    rearranged files to work ongithub

---
+++ js/config.base.js
@@ -0,0 +1,8 @@
+// Configuration file
+// This file should be ignored by version control for security reasons
+
+window.APP_CONFIG = {
+    "gist_id": "your id",
+    "gist_token": "your jey"
+  };
+  
