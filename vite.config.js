a309c3a74979ab8cfebd0d948fb6fc9b8c466ed2
ma rco <22920113-marcorossiIT@users.noreply.gitlab.com>
2024-12-15 06:19:58 +0100

    updated vite

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,14 @@ import framework7 from 'rollup-plugin-framework7';
 const SRC_DIR = path.resolve(__dirname, './src');
 const PUBLIC_DIR = path.resolve(__dirname, './public');
 const BUILD_DIR = path.resolve(__dirname, './www');
+
 export default async () => {
   return {
     plugins: [
       framework7({ emitCss: false }),
     ],
     root: SRC_DIR,
-    base: '/ialbums/', // Replace <your-repo-name> with the name of your GitLab repository
+    base: '/', // Use '/' as the base path
     publicDir: PUBLIC_DIR,
     build: {
       outDir: BUILD_DIR,
