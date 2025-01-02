a1a4a483a434d23aa8263fea922dc7d369d987bb
ma rco <22920113-marcorossiIT@users.noreply.gitlab.com>
2025-01-02 16:30:49 +0100

    storage solution renewed

--- src/js/store.js
+++ src/js/store.js
@@ -1,206 +1,146 @@
 import { createStore } from 'framework7';
 import remotedataConfig from '../remoteStorage.config';
+const crypto = require('crypto');
 
 
 const { dataUrl: remoteDataURL, apiKey: remoteDataAPIKey } = remotedataConfig;
 
 const localStorageKey = 'iAlbumsData';
 
+// ====== MOCK DATA ===== //
+const mockData = JSON.parse(`{
+  "albums": {
+    "h9dc9hds": {
+      "title": "album11",
+      "authors": [
+        "Black Country",
+        "New Road"
+      ],
+      "year": 1966,
+      "genre": "Rock",
+      "rating": "good",
+      "notes": "bla bla bla bla bla blab llaa",
+      "standouts": "2024"
+    },
+    "bj43kk34bk": {
+      "title": "Little Electric Chicken Heart",
+      "authors": [
+        "Ana Frango Electrico"
+      ],
+      "year": 2024,
+      "genre": "POP/LATIN POP/HIP HOP",
+      "rating": "good",
+      "notes": "bla bla bla bla bla blab llaa",
+      "standouts": "2024"
+    }
+  },
+  "lastModified": "2024-12-17T17:08:19.551Z"
+}`);
+const baseAlbum = {
+  "title": null,
+  "authors": null,
+  "year": null,
+  "genre": null,
+  "rating": null,
+  "notes": null,
+  "standouts": null
+}
+
+
+const getDataFromRemote = async () => {
+  /*
+  get from online;
+  return the data to the caller
+  */
+
+  const remoteResults = await fetch(remoteDataURL, {
+    headers: {
+      'X-Access-Key': remoteDataAPIKey,
+      'X-Bin-Meta': true,
+    },
+  });
+  if (!remoteResults.ok) {
+    throw new Error(`HTTP error! Status: ${remoteResults.status}`);
+  }
+  const { record: remoteAlbums } = await remoteResults.json();
+
+  return remoteAlbums
+
+
+}
+
 const store = createStore({
   state: {
-    albums: [],
-    authors: [],
-    ratings: [],
-    lastModified: null,
+    albums: {},
+    lastModified: 0,
   },
   getters: {
     albums({ state }) {
       return state.albums;
     },
-    authors({ state }) {
-      return state.authors;
-    },
-    ratings({ state }) {
-      return state.ratings;
-    },
+
   },
   actions: {
     // Adds a brand new album
     addAlbum({ state, dispatch }, newAlbum) {
-      state.albums.push(newAlbum);
-      state.lastModified = new Date().toISOString();
-      dispatch('saveToLocalStorage');
-      dispatch('pushData');
-    }, updateAlbum({ state, dispatch }, updatedAlbum) {
-      const albumIndex = state.albums.findIndex((a) => a.id === updatedAlbum.id);
-      if (albumIndex !== -1) {
-        state.albums[albumIndex] = { ...state.albums[albumIndex], ...updatedAlbum };
-        state.lastModified = new Date().toISOString();
-        dispatch('saveToLocalStorage');
-        dispatch('pushData');
-      }
-    },
 
-    addAuthor({ state, dispatch }, newAuthor) {
-      state.authors.push(newAuthor);
-      state.lastModified = new Date().toISOString();
-      dispatch('saveToLocalStorage');
-      dispatch('pushData');
-    },
 
-    addRating({ state, dispatch }, rating) {
-      if (!state.ratings.includes(rating)) {
-        state.ratings.push(rating);
-        state.lastModified = new Date().toISOString();
-        dispatch('saveToLocalStorage');
-        dispatch('pushData');
-      }
-    },
-    loadLocalStorage({ state }) {
-      const localData = JSON.parse(localStorage.getItem(localStorageKey)) || {};
-      state.albums = localData.albums || [];
-      state.authors = localData.authors || [];
-      state.ratings = localData.ratings || [];
-      state.lastModified = localData.lastModified || null;
-    },
-    saveToLocalStorage({ state }) {
-      const dataToSave = {
-        albums: state.albums,
-        authors: state.authors,
-        ratings: state.ratings,
-        lastModified: state.lastModified || new Date().toISOString(),
+      // prepare data
+      //   prepare album
+      let newAlbumDetails = {
+        title: newAlbum?.title || null,
+        authors: newAlbum?.authors || null,
+        year: newAlbum?.year || null,
+        genre: newAlbum?.genre || null,
+        rating: newAlbum?.rating || null,
+        notes: newAlbum?.notes || null,
+        standouts: newAlbum?.standouts || null
       };
-      localStorage.setItem(localStorageKey, JSON.stringify(dataToSave));
-    },
-    async fetchData({ dispatch, state }) {
-      try {
-        const remoteResult = await fetch(remoteDataURL, {
-          headers: {
-            'X-Access-Key': remoteDataAPIKey,
-            'X-Bin-Meta': true,
-          },
-        });
-        if (!remoteResult.ok) {
-          throw new Error(`HTTP error! Status: ${remoteResult.status}`);
-        }
-        const { record: remoteAlbums } = await remoteResult.json();
 
-        // ====== MOCK DATA ===== //
-        const mockData = JSON.parse(`{
-  "albums": [
-    {
-      "id": "<hash_1>",
-      "title": "album11",
-      "authors": [
-        "author_1734455284222"
-      ],
-      "rating": "goods"
-    },
-    {
-      "id": "<hash_1>",
-      "title": "Clbum11",
-      "authors": [
-        "author_1734455284222"
-      ],
-      "rating": "aah"
-    },
-    {
-      "id": "<hash_1>",
-      "title": "balbum2",
-      "authors": [
-        "author_1734455284222"
-      ],
-      "rating": "goods"
-    },
-    {
-      "id": "<hash_1>",
-      "title": "balbum11",
-      "authors": [
-        "author_1734445908308"
-      ],
-      "rating": "meh"
-    },
-    {
-      "id": "<hash_1>",
-      "title": "album11",
-      "authors": [
-        "author_1734407204797"
-      ],
-      "rating": "goods"
-    }
-  ],
-  "authors": [
-    {
-      "id": "author_1734407204797",
-      "name": "pippox"
-    },
-    {
-      "id": "author_unknown",
-      "name": "Unknown Author"
-    },
-    {
-      "id": "author_1734409305339",
-      "name": "ll"
-    },
-    {
-      "id": "author_1734445908308",
-      "name": "Lenin"
-    },
-    {
-      "id": "author_1734447416683",
-      "name": "aaafffaf"
-    },
-    {
-      "id": "author_1734448620243",
-      "name": "rino"
-    },
-    {
-      "id": "author_1734448647870",
-      "name": "Ryno"
-    },
-    {
-      "id": "author_1734455284222",
-      "name": "pippos"
-    }
-  ],
-  "ratings": [
-    "good",
-    "aa",
-    "meh",
-    "goods"
-  ],
-  "lastModified": "2024-12-17T17:08:19.551Z"
-}`);
+      // update local
+      //   edit last edit time
+      //   add album
+      state.albums[crypto.createHash('sha1').update(JSON.stringify(newAlbum)).digest('hex')] = newAlbumDetails;
+      state.lastModified = new Date().toISOString();
 
-        const localData = JSON.parse(localStorage.getItem(localStorageKey)) || {};
+      // optional check last edit time per conflitti 
 
-        const remoteTime = remoteAlbums.lastModified ? new Date(remoteAlbums.lastModified) : 0;
-        const localTime = localData.lastModified ? new Date(localData.lastModified) : 0;
+      // send data online
+      dispatch('updateRemote');
 
-        if (remoteTime > localTime) {
-          // Remote data is newer; use it
-          localStorage.setItem(localStorageKey, JSON.stringify(remoteAlbums));
-          dispatch('loadLocalStorage');
-        } else if (localTime > remoteTime) {
-          // Local data is newer; push it
-          dispatch('pushData');
-        } else {
-          // Both are same or no data, load local
-          dispatch('loadLocalStorage');
-        }
-      } catch (error) {
-        console.error('Error fetching remote data:', error);
-        dispatch('loadLocalStorage');
-      }
+    },
+    updateAlbum({ state, dispatch }, updatedAlbum) {
+
+      // prepare data
+      //   prepare album
+      let uppedAlbumDetails = {
+        title: updatedAlbum?.title || null,
+        authors: updatedAlbum?.authors || null,
+        year: updatedAlbum?.year || null,
+        genre: updatedAlbum?.genre || null,
+        rating: updatedAlbum?.rating || null,
+        notes: updatedAlbum?.notes || null,
+        standouts: updatedAlbum?.standouts || null
+      };
+
+      // update local
+      //   edit last edit time
+      //   update album
+      state.albums[uppedAlbumDetails.id] = uppedAlbumDetails; // TODO fare reactive
+
+      // optional check last edit time per conflitti
+
+      // send data online
+      dispatch('updateRemote');
+
+
     },
 
-    async pushData({ state }) {
+    async updateRemote({ state }) {
       try {
         const payload = {
           albums: state.albums,
-          authors: state.authors,
-          ratings: state.ratings,
-          lastModified: new Date().toISOString(),
+          lastModified: state.lastModified
         };
 
         const response = await fetch(remoteDataURL, {
@@ -217,12 +157,35 @@ const store = createStore({
         }
 
         console.log('Data successfully pushed to remote');
-        localStorage.setItem(localStorageKey, JSON.stringify(payload));
-        state.lastModified = payload.lastModified;
+
+
       } catch (error) {
         console.error('Error pushing data:', error);
       }
     },
+
+    async getRemote({ state }) {
+      try {
+        const remoteResult = await fetch(remoteDataURL, {
+          headers: {
+            'X-Access-Key': remoteDataAPIKey,
+            'X-Bin-Meta': true,
+          },
+        });
+        if (!remoteResult.ok) {
+          throw new Error(`HTTP error! Status: ${remoteResult.status}`);
+        }
+        const { record: remoteAlbums } = await remoteResult.json();
+        state.albums = { ...remoteAlbums.albums }
+
+
+      } catch (error) {
+        console.error('Error fetching remote data:', error);
+        $f7.toast.create({ text: 'Error with remote sync!', closeTimeout: 2000 }).open();
+
+      }
+    },
+
 
   },
 });
